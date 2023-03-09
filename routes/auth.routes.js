1f98b3a4168fc4fc51d40c4be2b6b5c4fc16a70e
Cristian Gomez <gmz.mrs.cristian@gmail.com>
2023-03-08 21:21:19 -0600

    Lista la ruta de autenticacion

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,6 +1,6 @@
 import express from "express";
-import { isAuthenticated } from '../middleware/jwt.middleware.js'
-import { loginCntlr, signupCntlr, verifyCntlr } from '../controllers/auth.controllers.js'
+import { isAuthenticated } from '../middleware/jwt.middleware.js';
+import { loginCntlr, signupCntlr, verifyCntlr } from '../controllers/auth.controllers.js';
 const router = express.Router();
 
 
